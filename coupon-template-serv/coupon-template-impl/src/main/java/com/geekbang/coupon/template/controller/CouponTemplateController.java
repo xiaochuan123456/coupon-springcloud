3f82da0b0abf0f39709f148e7fd07bbad17629ce
xiaolichuan <xiaolichuan@ruiyanai.com>
2022-11-23 18:28:45 +0800

    服务容错Sentinel

--- coupon-template-serv/coupon-template-impl/src/main/java/com/geekbang/coupon/template/controller/CouponTemplateController.java
+++ coupon-template-serv/coupon-template-impl/src/main/java/com/geekbang/coupon/template/controller/CouponTemplateController.java
@@ -1,10 +1,13 @@
 package com.geekbang.coupon.template.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.alibaba.fastjson.JSON;
 import com.geekbang.coupon.template.api.beans.CouponTemplateInfo;
 import com.geekbang.coupon.template.api.beans.PagedCouponTemplateInfo;
 import com.geekbang.coupon.template.api.beans.TemplateSearchParams;
 import com.geekbang.coupon.template.service.intf.CouponTemplateService;
+import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.sql.Template;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +41,7 @@ public class CouponTemplateController {
 
     // 读取优惠券
     @GetMapping("/getTemplate")
+    @SentinelResource(value = "getTemplate")
     public CouponTemplateInfo getTemplate(@RequestParam("id") Long id){
         log.info("Load template, id={}", id);
         return couponTemplateService.loadTemplateInfo(id);
@@ -45,9 +49,38 @@ public class CouponTemplateController {
 
     // 批量获取
     @GetMapping("/getBatch")
-    public Map<Long, CouponTemplateInfo> getTemplateInBatch(@RequestParam("ids") Collection<Long> ids) {
+    // 你也可以通过defaultFallback属性做一个全局限流、降级的处理逻辑
+    // 如果你不想将降级方法写在当前类里，可以通过blockHandlerClass和fallbackClass指定"降级类"
+    @SentinelResource(value = "getTemplateInBatch",
+            fallback = "getTemplateInBatch_fallback",
+            blockHandler = "getTemplateInBatch_block")
+    public Map<Long, CouponTemplateInfo> getTemplateInBatch(
+            @RequestParam("ids") Collection<Long> ids) {
+        // 如果接口被熔断，那么下面这行log不会被打印出来
         log.info("getTemplateInBatch: {}", JSON.toJSONString(ids));
+        // 可以测试异常比例、异常数熔断
+        if (ids.size() == 2) {
+            throw new RuntimeException("异常");
+        }
+        // 可以测试慢调用熔断
+//            try {
+//                Thread.sleep(500 * ids.size());
+//            } catch (Exception e) {
+//            }
         return couponTemplateService.getTemplateInfoMap(ids);
+    }
+
+    // 接口被降级时的方法
+    public Map<Long, CouponTemplateInfo> getTemplateInBatch_fallback(Collection<Long> ids) {
+        log.info("接口被降级");
+        return Maps.newHashMap();
+    }
+
+    // 流控、熔断降级的方法
+    public Map<Long, CouponTemplateInfo> getTemplateInBatch_block(
+            Collection<Long> ids, BlockException e) {
+        log.info("接口被限流");
+        return Maps.newHashMap();
     }
 
     // 搜索模板
